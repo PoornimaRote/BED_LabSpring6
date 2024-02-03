c71797933a04ef4553ca2d5ef1e27d07098d567b
PoornimaRote <142153202+PoornimaRote@users.noreply.github.com>
2024-02-03 17:38:49 +0530

    Update MySecurityConfig.java

--- StudentManagement/StudentManagement/src/main/java/com/grealearning/students/config/MySecurityConfig.java
+++ StudentManagement/StudentManagement/src/main/java/com/grealearning/students/config/MySecurityConfig.java
@@ -1,4 +1,4 @@
-package com.grealearning.students.config;
+package com.greatlearning.students.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.grealearning.students.service.UserDetailsServiceImpl;
+import com.greatlearning.students.service.UserDetailsServiceImpl;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
