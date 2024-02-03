27ee7500c3e853cf71a553c84fda94eacbf1d74c
PoornimaRote <142153202+PoornimaRote@users.noreply.github.com>
2024-02-03 17:43:45 +0530

    Update StudentManagementApplication.java

--- StudentManagement/StudentManagement/src/main/java/com/grealearning/students/StudentManagementApplication.java
+++ StudentManagement/StudentManagement/src/main/java/com/grealearning/students/StudentManagementApplication.java
@@ -1,4 +1,4 @@
-package com.grealearning.students;
+package com.greatlearning.students;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
