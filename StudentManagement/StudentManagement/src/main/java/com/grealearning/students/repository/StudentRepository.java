74939c08e9cad07d61eb8b8ddae3acd8ee3be01f
PoornimaRote <142153202+PoornimaRote@users.noreply.github.com>
2024-02-03 17:42:15 +0530

    Update StudentRepository.java

--- StudentManagement/StudentManagement/src/main/java/com/grealearning/students/repository/StudentRepository.java
+++ StudentManagement/StudentManagement/src/main/java/com/grealearning/students/repository/StudentRepository.java
@@ -1,8 +1,8 @@
-package com.grealearning.students.repository;
+package com.greatlearning.students.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.grealearning.students.model.Student;
+import com.greatlearning.students.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
