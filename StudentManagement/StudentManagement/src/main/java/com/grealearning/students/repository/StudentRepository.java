a7186cad2d4b98c712645ee95db98b785f3c6bd1
PoornimaRote <142153202+PoornimaRote@users.noreply.github.com>
2024-02-04 17:13:06 +0530

    Delete StudentManagement directory

--- StudentManagement/StudentManagement/src/main/java/com/grealearning/students/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.students.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.students.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
