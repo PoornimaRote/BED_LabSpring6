bb99be5ca35689e459dbea1dec7af610ae8e86fc
PoornimaRote <142153202+PoornimaRote@users.noreply.github.com>
2024-02-04 21:32:16 +0530

    Update StudentRepository.java

--- students/src/main/java/com/greatlearning/students/repository/StudentRepository.java
+++ students/src/main/java/com/greatlearning/students/repository/StudentRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.greatlearning.students.model.Student;
 import com.greatlearning.students.model.User;
 
-public interface StudentRepository extends JpaRepository<User, Integer> {
-
-	@Query("select u from User u where u.username = ?1")
-	User findByUsername(String username);
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
 }
